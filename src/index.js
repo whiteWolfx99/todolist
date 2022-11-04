4dc61a4bbd6d26569f7381581bc9486fda2c5780
whiteWolfx99 <hhqu180099@uniq.edu.iq>
2022-11-04 15:08:28 +0300

    fixing linters

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
-import "./style.css";
+import './style.css';
 
-const tomake = document.getElementById("firstidoftodo");
+const tomake = document.getElementById('firstidoftodo');
 
-let Listiftodo =
-  localStorage.getItem("containertodo") !== null
-    ? JSON.parse(localStorage.getItem("containertodo"))
-    : [];
-let Id =
-  Listiftodo.length > 0 ? Listiftodo[Listiftodo.length - 1].index + 1 : 0;
-const inputs = document.getElementById("inputvalue");
-const btns = document.getElementById("addlist");
-
+const Listiftodo = localStorage.getItem('containertodo') !== null
+  ? JSON.parse(localStorage.getItem('containertodo'))
+  : [];
+let Id = Listiftodo.length > 0 ? Listiftodo[Listiftodo.length - 1].index + 1 : 0;
+const inputs = document.getElementById('inputvalue');
+const btns = document.getElementById('addlist');
 
 function displaytodolist() {
-  tomake.innerHTML = "";
+  tomake.innerHTML = '';
   let count;
 
   for (count = 0; count < Listiftodo.length; count += 1) {
-    let checkiftrue = Listiftodo[count].completed === true ? "checked" : "";
+    const checkiftrue = Listiftodo[count].completed === true ? 'checked' : '';
     tomake.innerHTML += `
      <div class="containertodo">
        <div class="backtodo">
@@ -40,46 +37,40 @@ function displaytodolist() {
   }
   for (let i = 0; i < document.querySelectorAll('.remove').length; i += 1) {
     document.querySelectorAll('.remove')[i].addEventListener('click', () => {
-        if (confirm('Are you sure you want to delete this item?')) {
-            Listiftodo.splice(i, 1);
-            localStorage.setItem('containertodo', JSON.stringify(Listiftodo));
-            displaytodolist();
-        }
+      Listiftodo.splice(i, 1);
+      localStorage.setItem('containertodo', JSON.stringify(Listiftodo));
+      displaytodolist();
     });
   }
   for (let i = 0; i < document.querySelectorAll('.edit').length; i += 1) {
     document.querySelectorAll('.edit')[i].addEventListener('click', () => {
-        document.querySelectorAll('.change')[i].classList.remove('hidden');
-        document.querySelectorAll('.removebtn')[i].classList.remove('hidden');
-        document.querySelectorAll('.edit')[i].classList.add('hidden');
-        document.querySelectorAll('.description')[i].classList.add('hidden');
-        document.querySelectorAll('.editinputchange')[i].addEventListener('keypress', (e) => {
-            if (e.key === 'Enter') {
-                Listiftodo[i].description = document.querySelectorAll('.editinputchange')[i].value;
-                localStorage.setItem('containertodo', JSON.stringify(Listiftodo));
-                displaytodolist();
-            }
-        });
+      document.querySelectorAll('.change')[i].classList.remove('hidden');
+      document.querySelectorAll('.removebtn')[i].classList.remove('hidden');
+      document.querySelectorAll('.edit')[i].classList.add('hidden');
+      document.querySelectorAll('.description')[i].classList.add('hidden');
+      document.querySelectorAll('.editinputchange')[i].addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
+          Listiftodo[i].description = document.querySelectorAll('.editinputchange')[i].value;
+          localStorage.setItem('containertodo', JSON.stringify(Listiftodo));
+          displaytodolist();
+        }
+      });
     });
   }
 }
 
-
-
-btns.addEventListener("click", (e) => {
+btns.addEventListener('click', (e) => {
   e.preventDefault();
   const newvalue = document.getElementById('inputvalue').value;
-  inputs.value = "";
+  inputs.value = '';
   Id += 1;
   Listiftodo.push({
-    Id: Id,
+    Id,
     description: newvalue,
     completed: false,
   });
-  localStorage.setItem("containertodo", JSON.stringify(Listiftodo));
+  localStorage.setItem('containertodo', JSON.stringify(Listiftodo));
   displaytodolist();
 });
 
-
-
-document.addEventListener("DOMContentLoaded", displaytodolist, false);
+document.addEventListener('DOMContentLoaded', displaytodolist, false);
