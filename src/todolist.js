bbcf6391360c56c0b7f46328e2c7dcd032a2f491
Paulina Gonzalez <paulinagzzc@gmail.com>
2022-11-09 15:27:49 -0500

    Change function to for each

--- src/todolist.js
+++ src/todolist.js
@@ -1,39 +1,43 @@
 const tomake = document.getElementById('firstidoftodo');
 const clearall = document.getElementById('clearbutton');
 
-const Listiftodo = localStorage.getItem('containertodo') !== null
-  ? JSON.parse(localStorage.getItem('containertodo'))
-  : [];
-let Id = Listiftodo.length > 0 ? Listiftodo[Listiftodo.length - 1].index + 1 : 0;
+const Listiftodo =
+  localStorage.getItem('containertodo') !== null
+    ? JSON.parse(localStorage.getItem('containertodo'))
+    : [];
+let Id =
+  Listiftodo.length > 0 ? Listiftodo[Listiftodo.length - 1].index + 1 : 0;
 const inputs = document.getElementById('inputvalue');
 const btns = document.getElementById('addlist');
 
-function displaytodolist() {
+const displaytodolist = () => {
   tomake.innerHTML = '';
-  let count;
 
-  for (count = 0; count < Listiftodo.length; count += 1) {
-    const checkiftrue = Listiftodo[count].completed === true ? 'checked' : '';
-    tomake.innerHTML += `
-     <div class="containertodo">
-       <div class="backtodo">
-         <input type="checkbox" class="check" name="completed" value="${Listiftodo[count].completed}" ${checkiftrue}>
-         <p class="description">${Listiftodo[count].description}</p>
-         <div class="change hidden">
-         <input class="editinputchange" value="${Listiftodo[count].description}">
-         </div>
-       </div>
-       <div class="flex-end">
-         <div id="removebtn" class="removebtn hidden">
-           <button id="remove" class="remove"><i class='fa fa-trash'></i></button>
-         </div>
-         <div id="makechange" class="makechange">
-         <button id="edit" class="edit" type="button" value="${Listiftodo[count].index}">...</button>
-       </div>
-       </div>
-     </div>
-     `;
+  if (Listiftodo !== null) {
+    Listiftodo.forEach((Listiftodo) => {
+      const checkiftrue = Listiftodo.completed === true ? 'checked' : '';
+      tomake.innerHTML += `
+      <div class="containertodo">
+        <div class="backtodo">
+          <input type="checkbox" class="check" name="completed" value="${Listiftodo.completed}" ${checkiftrue}>
+          <p class="description">${Listiftodo.description}</p>
+          <div class="change hidden">
+          <input class="editinputchange" value="${Listiftodo.description}">
+          </div>
+        </div>
+        <div class="flex-end">
+          <div id="removebtn" class="removebtn hidden">
+            <button id="remove" class="remove"><i class='fa fa-trash'></i></button>
+          </div>
+          <div id="makechange" class="makechange">
+          <button id="edit" class="edit" type="button" value="${Listiftodo.index}">...</button>
+        </div>
+        </div>
+      </div>
+      `;
+    });
   }
+
   for (let i = 0; i < document.querySelectorAll('.remove').length; i += 1) {
     document.querySelectorAll('.remove')[i].addEventListener('click', () => {
       Listiftodo.splice(i, 1);
@@ -47,13 +51,16 @@ function displaytodolist() {
       document.querySelectorAll('.removebtn')[i].classList.remove('hidden');
       document.querySelectorAll('.edit')[i].classList.add('hidden');
       document.querySelectorAll('.description')[i].classList.add('hidden');
-      document.querySelectorAll('.editinputchange')[i].addEventListener('keypress', (e) => {
-        if (e.key === 'Enter') {
-          Listiftodo[i].description = document.querySelectorAll('.editinputchange')[i].value;
-          localStorage.setItem('containertodo', JSON.stringify(Listiftodo));
-          displaytodolist();
-        }
-      });
+      document
+        .querySelectorAll('.editinputchange')
+        [i].addEventListener('keypress', (e) => {
+          if (e.key === 'Enter') {
+            Listiftodo[i].description =
+              document.querySelectorAll('.editinputchange')[i].value;
+            localStorage.setItem('containertodo', JSON.stringify(Listiftodo));
+            displaytodolist();
+          }
+        });
     });
   }
   for (let i = 0; i < document.querySelectorAll('.check').length; i += 1) {
@@ -67,7 +74,7 @@ function displaytodolist() {
       displaytodolist();
     });
   }
-}
+};
 
 clearall.addEventListener('click', () => {
   for (let i = 0; i < Listiftodo.length; i += 1) {
